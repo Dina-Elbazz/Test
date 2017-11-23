678f1c6b972958a80733662fc658c8de2353b24a
NM-EG\Dina.ElBaz <dina.elbaz@nm-eg.com>
2017-11-23 15:19:58 +0200

    initial!

---
+++ GitExample/src/Data.java
@@ -0,0 +1,9 @@
+
+public class Data {
+	int data[] = {0,4,42};
+	
+	public void display(){
+		System.out.println("Data Display");
+	}
+
+}
---
+++ GitExample/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Data data = new Data();
+		
+		displayGreeting();
+		data.display();
+		
+
+	}
+	private static void displayGreeting(){
+		System.out.println("Helloo!!");
+		System.out.println("This app shows data");
+	}
+
+}
