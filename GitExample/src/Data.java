b177ead7825c1ff10886d4e20fd13ec8a5d0520e
NM-EG\Dina.ElBaz <dina.elbaz@nm-eg.com>
2017-11-25 13:53:48 +0200

    partial implementation

--- GitExample/src/Data.java
+++ GitExample/src/Data.java
@@ -3,7 +3,9 @@ public class Data {
 	int data[] = {0,4,42};
 	
 	public void display(){
-		System.out.println("Data Display");
+		for(int i=0;i<data.length;i++){
+			System.out.println(" ");
+		}
 	}
 
 }
